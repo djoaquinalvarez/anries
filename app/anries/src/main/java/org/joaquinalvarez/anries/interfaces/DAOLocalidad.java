7d172658fb6fc2cd30bc91932e536996d94ff9a0
Joaquin Alvarez <alvarezdjoaquin@gmail.com>
2023-02-16 17:24:03 -0300

    Interfaces DAO: implementamos la interfaz DAO para la clase Localidad

---
+++ app/anries/src/main/java/org/joaquinalvarez/anries/interfaces/DAOLocalidad.java
@@ -0,0 +1,14 @@
+package org.joaquinalvarez.anries.interfaces;
+
+import org.joaquinalvarez.anries.model.Localidad;
+
+import java.util.List;
+
+public interface DAOLocalidad {
+
+    public void registrar(Localidad localidad) throws Exception;
+    public void modificar(Localidad localidad) throws Exception;
+    public void eliminar(Localidad localidad) throws Exception;
+    public List<Localidad> listar() throws Exception;
+
+}
